5000d41f5c5e685033e7f46e6c23b61a365d07a5
Geffery Dopp <geffdroid@gmail.com>
2018-10-05 15:24:22 +0700

    ...

--- include/argon_cross.h
+++ include/argon_cross.h
@@ -1,6 +1,7 @@
 #include <math>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 class LegendrePolynom //uses recurrence relation, plus saves values for 2 maximum ls for faster evaluation of the next l (faster performance for the same x and rising l)
 {
@@ -14,6 +15,31 @@ protected:
 public:
 	LegendrePolynom();
 	long double operator ()(long double x, unsigned int l);
+};
+
+class LargeFactorHelper //assumed that only numbers less than N are in the input
+{
+protected:
+    unsigned int n_max;
+    void SimplifyFactorial(void);
+	//void Factorize(void);
+	void Simplify(void);
+	std::vector<unsigned int> dividers;
+	std::vector<unsigned int> denominators;
+	std::vector<unsigned int> prime_dividers; //according to the prime_list
+	std::vector<unsigned int> prime_denominators; //according to the prime_list
+	std::vector<unsigned int> prime_list; //all prime numbers <= n_max, starting from 2
+	std::vector<unsigned int> factor_dividers;
+	std::vector<unsigned int> factor_denominators;
+public:
+	LargeFactorHelper(unsigned int N_max);
+	void MultiplyByFactorial(unsigned int n);
+	void DivideByFactorial(unsigned int n);
+	void MultiplyBy(unsigned int n, unsigned int power = 1);
+	void DivideBy(unsigned int n, unsigned int power = 1);
+	long double Output(void);
+	void Print(void);
+	void Clear(void);
 };
 
 //E in eV, theta in radians, output is in cm-17
--- src/argon_cross.cpp
+++ src/argon_cross.cpp
@@ -47,6 +47,152 @@ long double LegendrePolynom::operator ()(long double x, unsigned int l) {
 	return P_last;
 }
 
+void LargeFactorHelper::SimplifyFactorial(void)
+{
+	while (true) {
+		unsigned int divider_max_factor = 1;
+		unsigned int divider_max_index = 0;
+		unsigned int denominator_max_factor = 1;
+		unsigned int denominator_max_index = 0;
+		for (unsigned int i = 0, i_end = factor_dividers.size(); i<i_end; ++i) {
+			divider_max_factor = std::max(divider_max_factor, factor_dividers[i]);
+			divider_max_index = i;
+		}
+		for (unsigned int i = 0, i_end = factor_dividers.size(); i<i_end; ++i) {
+			denominator_max_factor = std::max(denominator_max_factor, factor_denominators[i]);
+			denominator_max_index = i;
+		}
+		if ((1!=divider_max_factor)&&(1!=denominator_max_factor)) {
+			factor_dividers.erase(factor_dividers.begin() + divider_max_index);
+			factor_denominators.erase(factor_denominators.begin() + denominator_max_index);
+			for (unsigned int n = divider_max_factor; n>denominator_max_factor;--n)
+				MultiplyBy(n);
+			for (unsigned int n = denominator_max_factor; n>divider_max_factor;--n)
+				DivideBy(n);
+			continue;
+		}
+		for (unsigned int n = divider_max_factor; n>1;--n)
+			MultiplyBy(n);
+		for (unsigned int n = denominator_max_factor; n>1;--n)
+			DivideBy(n);
+		return;
+	}
+}
+
+/*void LargeFactorHelper::Factorize(void)
+{
+	for (unsigned int i = 0, i_end = dividers.size(); i!=i_end; ++i)
+		for (unsigned int pr = 0, pr_end = prime_list.size(); pr!=pr_end; ++pr) {
+			while (dividers[i]%prime_list[pr]==0) {
+				++prime_dividers[pr];
+				dividers[i]/=prime_list[pr];
+			}
+			if (1==dividers[i])
+				break;
+		}
+	for (unsigned int i = 0, i_end = denominators.size(); i!=i_end; ++i)
+		for (unsigned int pr = 0, pr_end = prime_list.size(); pr!=pr_end; ++pr) {
+			while (denominators[i]%prime_list[pr]==0) {
+				++prime_denominators[pr];
+				denominators[i]/=prime_list[pr];
+			}
+			if (1==denominators[i])
+				break;
+		}
+	denominators.erase(denominators.begin(),denominators.end());
+	dividers.erase(dividers.begin(),dividers.end());
+}*/
+
+void LargeFactorHelper::Simplify(void)
+{
+	for (unsigned int pr = 0, pr_end = prime_list.size(); pr!=pr_end; ++pr) {
+		if (prime_denominators[pr]>=prime_dividers[pr]){
+			prime_denominators[pr] -= prime_dividers[pr];
+			prime_dividers[pr] = 0;
+		} else {
+			prime_dividers[pr] -= prime_denominators[pr];
+			prime_denominators[pr] = 0;
+		}
+	}
+}
+
+LargeFactorHelper::LargeFactorHelper(unsigned int N_max)
+{
+	n_max =	N_max > 2 ? N_max : 2;
+	for (unsigned int i = 2; i<=n_max; ++i) {
+		bool prime = true;
+		for (unsigned int pr = 0; pr<prime_list.size(); ++pr) {
+			if (0==i%prime_list[pr]) {
+				prime = false;
+				break;
+			}
+		}
+		if (prime)
+			prime_list.push_back(i);
+	}
+}
+
+void LargeFactorHelper::MultiplyByFactorial(unsigned int n)
+{
+	if (n>n_max) {
+		std::cout<<"LargeFactorHelper::MultiplyByFactorial() error: "<<n<<" is larger than n_max = "<<n_max<<std::endl;
+		return;
+	}
+	factor_dividers.push_back(n);
+}
+
+void LargeFactorHelper::DivideByFactorial(unsigned int n)
+{
+	if (n>n_max) {
+		std::cout<<"LargeFactorHelper::DivideByFactorial() error: "<<n<<" is larger than n_max = "<<n_max<<std::endl;
+		return;
+	}
+	factor_denominators.push_back(n);
+}
+
+void LargeFactorHelper::MultiplyBy(unsigned int n, unsigned int power)
+{
+	if (power<1)
+		return;
+	if (n>n_max) {
+		std::cout<<"LargeFactorHelper::MultiplyBy() error: "<<n<<" is larger than n_max = "<<n_max<<std::endl;
+		return;
+	}
+	for (unsigned int pr = 0, pr_end = prime_list.size(); pr!=pr_end; ++pr) {
+		while (n%prime_list[pr]==0) {
+			prime_dividers[pr]+=power;
+			n/=prime_list[pr];
+		}
+		if (1==n)
+			break;
+	}
+}
+
+void LargeFactorHelper::DivideBy(unsigned int n, unsigned int power)
+{
+	if (power<1)
+		return;
+	if (n>n_max) {
+		std::cout<<"LargeFactorHelper::MultiplyBy() error: "<<n<<" is larger than n_max = "<<n_max<<std::endl;
+		return;
+	}
+	for (unsigned int pr = 0, pr_end = prime_list.size(); pr!=pr_end; ++pr) {
+		while (n%prime_list[pr]==0) {
+			prime_denominators[pr]+=power;
+			n/=prime_list[pr];
+		}
+		if (1==n)
+			break;
+	}
+}
+
+long double LargeFactorHelper::Output(void);
+
+void LargeFactorHelper::Print(void);
+
+void LargeFactorHelper::Clear(void);
+
+
 long double argon_cross_elastic_diff (long double E, long double theta) {
 	if (E <= 1) {
 
